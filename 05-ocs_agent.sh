233efcec5d7bf3c05ad83210998cc986310f0f2c
Márcio Aquilles <marcioaquilles@yahoo.com.br>
2022-07-06 13:43:33 -0300

    Update 05-ocs_agent.sh

--- 05-ocs_agent.sh
+++ 05-ocs_agent.sh
@@ -28,14 +28,14 @@ export DEBIAN_FRONTEND="noninteractive"
 # opção do comando if: [ ] = teste de expressão, && = operador lógico AND, == comparação de string, exit 1 = 
 # A maioria dos erros comuns na execução
 clear
-if [ "$USUARIO" == "0" ] && [ "$UBUNTU" == "16.04" ] && [ "$KERNEL" == "4.4" ]
+if [ "$USUARIO" == "0" ] && [ "$UBUNTU" == "20.04" ] && [ "$KERNEL" == "5.4" ]
 	then
 		echo -e "O usuário é Root, continuando com o script..."
-		echo -e "Distribuição é >=16.04.x, continuando com o script..."
-		echo -e "Kernel é >= 4.4, continuando com o script..."
+		echo -e "Distribuição é >=20.04.x, continuando com o script..."
+		echo -e "Kernel é >= 5.4, continuando com o script..."
 		sleep 5
 	else
-		echo -e "Usuário não é Root ($USUARIO) ou Distribuição não é >=16.04.x ($UBUNTU) ou Kernel não é >=4.4 ($KERNEL)"
+		echo -e "Usuário não é Root ($USUARIO) ou Distribuição não é >=20.04.x ($UBUNTU) ou Kernel não é >=5.4 ($KERNEL)"
 		echo -e "Caso você não tenha executado o script com o comando: sudo -i"
 		echo -e "Execute novamente o script para verificar o ambiente."
 		exit 1
